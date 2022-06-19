56d51eb00af4a59335de9459b105a53403c036b2
chandru <chandru9279@users.noreply.github.com>
2022-06-20 01:28:33 +0530

    formatting changes

--- spyplane/main.py
+++ spyplane/main.py
@@ -1,7 +1,7 @@
-from spyplane.constants import TOKEN
-from spyplane.spy_plane import bot
 from spyplane.commands import Commands
+from spyplane.constants import TOKEN
 from spyplane.listeners import Listeners
+from spyplane.spy_plane import bot
 
 
 def run():
