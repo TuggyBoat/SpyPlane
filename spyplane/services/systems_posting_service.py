68d2dc781440e054cb7570908f48196387d46413
chandru <chandru9279@users.noreply.github.com>
2022-06-19 17:14:40 +0530

    daily sequence for list splits

--- spyplane/services/systems_posting_service.py
+++ spyplane/services/systems_posting_service.py
@@ -1,8 +1,8 @@
+import datetime
 from typing import List, Dict, cast
 
 import aiosqlite
 import discord.message
-import datetime
 
 from spyplane.constants import EMOJI_BULLSEYE, DB_PATH, FACTION_SCOUT_ROLE_ID
 from spyplane.database.systems_repository import SystemsRepository
@@ -15,6 +15,8 @@ class SystemsPostingService:
     def __init__(self, channel):
         self.channel = channel
 
+    start_date = datetime.date(2022, 6, 18)  # start day randomly chosen for the daily sequence
+
     async def publish_systems_to_scout(self):
         repo = SystemsRepository()
         async with aiosqlite.connect(DB_PATH) as db:
@@ -22,7 +24,7 @@ class SystemsPostingService:
             valid_systems = await repo.get_valid_systems()
         emoji_bullseye = bot.get_emoji(EMOJI_BULLSEYE)
         await self.channel.purge(check=is_not_pinned_message)
-        splits = split_valid_systems(valid_systems, datetime.datetime.today().weekday())
+        splits = split_valid_systems(valid_systems, (datetime.date.today() - self.start_date).days)
         await self.post_list(emoji_bullseye, splits, 'Primary')
         await self.post_list(emoji_bullseye, splits, 'Secondary')
         await self.post_list(emoji_bullseye, splits, 'Tertiary')
@@ -42,7 +44,7 @@ def is_not_pinned_message(message: discord.message.Message) -> bool:
     return not message.pinned
 
 
-def split_valid_systems(systems: List[ScoutSystem], day_of_week: int) -> Dict[str, List[ScoutSystem]]:
+def split_valid_systems(systems: List[ScoutSystem], daily_sequence: int) -> Dict[str, List[ScoutSystem]]:
     # TODO: use a db sequence instead of day of week.
     splits = {
         'Primary': [s for s in systems if int(s.priority)==1],
@@ -53,8 +55,8 @@ def split_valid_systems(systems: List[ScoutSystem], day_of_week: int) -> Dict[st
     every_third_day = list(split(splits['Tertiary'], 3))
     return {
         'Primary': splits['Primary'],
-        'Secondary': cast(List[ScoutSystem], every_other_day[day_of_week % 2]),
-        'Tertiary': cast(List[ScoutSystem], every_third_day[day_of_week % 3])
+        'Secondary': cast(List[ScoutSystem], every_other_day[daily_sequence % 2]),
+        'Tertiary': cast(List[ScoutSystem], every_third_day[daily_sequence % 3])
     }
 
 
