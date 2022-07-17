537efa9de1d1db83f8e97b8e2a4e49f3afc9b94d
chandru <chandru9279@users.noreply.github.com>
2022-07-18 00:16:32 +0530

    ebgs system query

---
+++ spyplane/services/ebgs_service.py
@@ -0,0 +1,33 @@
+import aiohttp
+import logging
+from typing import Optional
+
+import jq
+
+from spyplane.constants import log
+
+faction_active_state_query = '''
+    .docs[].factions[]
+    | select ((.faction_details.faction_presence.active_states|length > 0) or (.faction_details.faction_presence.pending_states|length > 0))    
+    | { "Name": (.name), "Active":  (.faction_details.faction_presence.active_states | map(.state) | join(",")), "Pending":  (.faction_details.faction_presence.pending_states | map(.state) | join(",")) }
+    '''
+
+#  
+class EliteBgsService:
+    def __init__(self):
+        self.compiled_faction_active_state_query = jq.compile(faction_active_state_query)
+
+    async def get_system_faction_not_none_states(self, system):
+        params = {
+            "name": system,
+            "page": 1,
+            "factionDetails": "true"
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://elitebgs.app/api/ebgs/v5/systems', params=params) as r:
+                faction_history_json_parsed = await r.json()
+        # print(faction_history_json_parsed, flush=True)
+        compile_input = self.compiled_faction_active_state_query.input(faction_history_json_parsed)
+        results = compile_input.all()
+        faction_history = [result for result in results]
+        return faction_history
---
+++ tests/test_ebgs_service.py
@@ -0,0 +1,24 @@
+from typing import List
+from unittest import IsolatedAsyncioTestCase, mock
+from test_data import test_data
+
+from spyplane.services.ebgs_service import EliteBgsService
+
+
+class EliteBgsServiceTests(IsolatedAsyncioTestCase):
+
+    async def asyncSetUp(self) -> None:
+        self.subject = EliteBgsService()
+
+    async def asyncTearDown(self):
+        pass
+
+    @mock.patch('aiohttp.ClientSession.get', side_effect=test_data.mocked_requests_get)
+    async def test_get_system_faction_not_none_states_all_none(self, mocked_get):
+        json = await self.subject.get_system_faction_not_none_states('Wally Bei')
+        self.assertEqual(len(json), 0)
+
+    @mock.patch('aiohttp.ClientSession.get', side_effect=test_data.mocked_requests_get)
+    async def test_get_system_faction_not_none_states_some_interesting(self, mocked_get):
+        json = await self.subject.get_system_faction_not_none_states('Beatis')
+        self.assertEqual(len(json), 2)
